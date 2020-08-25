73ac755d9c487d9cb5656daca743fbc5c9eff3fa
declan baldwin <dbaldwin4@boxfusionconsulting.com>
2020-08-25 14:15:56 +0100

    implement animated scrolling

--- src/Authentication/Onboarding/Onboarding.tsx
+++ src/Authentication/Onboarding/Onboarding.tsx
@@ -1,10 +1,127 @@
-import React from "react";
-import { View } from "react-native";
+import React, { useRef } from "react";
+import { View, StyleSheet, Dimensions } from "react-native";
+// import { ScrollView } from "react-native-gesture-handler";
+import Animated, { multiply } from "react-native-reanimated";
+import { useValue, onScrollEvent, interpolateColor } from "react-native-redash";
+import Slide, { SLIDE_HEIGHT } from "./Slide";
+import SubSlide from "./SubSlide";
 
-interface ComponentNameProps {}
+const BORDER_RADIUS = 75;
+const { width } = Dimensions.get("window");
 
-const ComponentName = () => {
-  return <View style={{ flex: 1, backgroundColor: "cyan" }} />;
+const slides = [
+  {
+    title: "Relaxed",
+    color: "#BFEAF5",
+    subtitle: "Find Your Outfits",
+    description:
+      "Confused about your outfit? Don’t worry! Find the best outfit here!",
+  },
+  {
+    title: "Playful",
+    color: "#BEECC4",
+    subtitle: "Hear it First, Wear it First",
+    description:
+      "Hating the clothes in your wardrobe? Explore hundreds of outfit ideas",
+  },
+  {
+    title: "Excentric",
+    color: "#FFE4D9",
+    subtitle: "Your Style, Your Way",
+    description:
+      " Create your individual & unique style and look amazing everyday",
+  },
+  {
+    title: "Funky",
+    color: "#FFDDDD",
+    subtitle: "Look Good, Feel Good",
+    description:
+      "Discover the latest trends in fashion and explore your personality",
+  },
+];
+
+const Onboarding = () => {
+  const scroll = useRef<Animated.ScrollView>(null);
+  const x = useValue(0);
+
+  // TOTOD: scrollHandler useScrollHandler?
+  const onScroll = onScrollEvent({ x });
+  const backgroundColor = interpolateColor(x, {
+    inputRange: slides.map((_slide, i) => i * width),
+    outputRange: slides.map((slide) => slide.color),
+  });
+  return (
+    <View style={styles.container}>
+      <Animated.View style={[styles.slider, { backgroundColor }]}>
+        <Animated.ScrollView
+          ref={scroll}
+          horizontal
+          snapToInterval={width}
+          decelerationRate="fast"
+          showsHorizontalScrollIndicator={false}
+          bounces={false}
+          scrollEventThrottle={1}
+          {...{ onScroll }}
+        >
+          {slides.map((slide, i) => (
+            <Slide key={i} title={slide.title} right={!!(i % 2)} />
+          ))}
+        </Animated.ScrollView>
+      </Animated.View>
+      <View style={styles.footer}>
+        <Animated.View
+          style={{ ...StyleSheet.absoluteFillObject, backgroundColor }}
+        />
+        <Animated.View
+          style={[
+            styles.flexContent,
+            {
+              width: width * slides.length,
+              flex: 1,
+              transform: [{ translateX: multiply(x, -1) }],
+            },
+          ]}
+        >
+          {slides.map((slide, i) => (
+            <SubSlide
+              key={i}
+              onPress={() => {
+                if (scroll.current) {
+                  scroll.current
+                    .getNode()
+                    .scrollTo({ x: width * (i + 1), animated: true });
+                }
+              }}
+              last={i === slides.length - 1}
+              subtitle={slide.subtitle}
+              description={slide.description}
+            />
+          ))}
+        </Animated.View>
+      </View>
+    </View>
+  );
 };
 
-export default ComponentName;
+export default Onboarding;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+  },
+  slider: {
+    height: SLIDE_HEIGHT,
+    backgroundColor: "cyan",
+    borderBottomRightRadius: BORDER_RADIUS,
+  },
+  flexContent: {
+    flex: 1,
+    flexDirection: "row",
+    backgroundColor: "white",
+    borderTopLeftRadius: BORDER_RADIUS,
+  },
+  footer: {
+    flex: 1,
+  },
+});
---
+++ src/Authentication/Onboarding/SubSlide.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Button } from "../../components";
+
+interface SubSlideProps {
+  subtitle: string;
+  description: string;
+  last?: boolean;
+  onPress: () => void;
+}
+
+const SubSlide = ({ description, subtitle, last, onPress }: SubSlideProps) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.subtitle}>{subtitle}</Text>
+      <Text style={styles.description}>{description}</Text>
+      <Button label={last ? "Let's get started" : "Next"} variant={last ? "primary" : "default"} {...{ onPress }}/>
+    </View>
+  );
+};
+
+export default SubSlide;
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 44,
+    flex: 1,
+  },
+  subtitle: {
+    fontFamily: "SFProDisplay-Bold",
+    fontSize: 24,
+    lineHeight: 30,
+    marginBottom: 12,
+    color: "#0C0D34",
+    textAlign: "center",
+  },
+  description: {
+    fontFamily: "SFProDisplay-Regular",
+    fontSize: 16,
+    lineHeight: 24,
+    color: "#0C0D34",
+    textAlign: "center",
+    marginBottom: 40
+  },
+});
